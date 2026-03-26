5e88281c705d1beaef720486880674a40ff6bed4
David Broczkowski <david.broczkowski@gmail.com>
2026-03-26 14:32:34 -0400

    added .ejs pages and functionality for login page and redirects for create player to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // Create an instance of an Express application. This app object will be used to define routes and middleware.
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 // Define a constant for the port number on which the server will listen.
 const PORT = 3000;
@@ -16,9 +17,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const PlayerModel = require('./models/playerModel');
+const playerModel = new PlayerModel();
+
 // Set up a route handler for GET requests to the root URL ('/').
-app.get('/hello', (req, res) => {
-  res.redirect('/index.html');
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/authenticate', (req, res) => {
+  const username = req.body.username;
+
+  const user_id = playerModel.get_player_id(username);
+
+  if(user_id == null){
+    console.log(`[ERROR] Player with username ${username} not found in Players.`);
+    res.redirect("/create_player");
+    return;
+  }
+
+  res.redirect("/home");
+});
+
+app.get('/create_player', (req, res) => {
+  res.render("create_player");
+});
+
+app.get('/home', (req, res) => {
+  res.redirect("/index.html");
 });
 
 // Start the server and make it listen on the specified port.
@@ -28,11 +54,34 @@ app.listen(PORT, () => {
 });
 
 
-// -----------------------------------------------------------
-const PlayerModel = require('./models/playerModel');
-const playerModel = new PlayerModel();
 
-app.get('/submit', (req, res) =>{
-  playerModel.create("lab8", "lab8");
+// David Game ------------------------------------------------------------
+const GamePlayModel = require('./models/gamePlayModel');
+const gamePlayModel = new GamePlayModel();
+
+app.get('/david', (req, res, next) => {
+  res.render('david_game');
+});
+
+app.post('/david_submit', (req, res) => {
+  const username = req.body.username;
+  const score = req.body.score;
+
+  const user_id = playerModel.get_player_id(username);
+
+  if(user_id == null){
+    console.log(`[ERROR] Player with username ${username} not found in Players.`);
+    return; 
+    //have user create new profile and then try and resubmit info again?
+  }
+
+  try{
+    gamePlayModel.create(user_id, 4, score);
+  } catch (e){
+    console.log(e);
+    console.log("[ERROR] An error occurred while adding the record to the GamePlay relation.");
+  }
+
   res.redirect('/index.html');
 });
+
